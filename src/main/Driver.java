1d79ce5b782716aaf99029c8ea67b302b62edfa0
Oleks Korshak <alexokorshak@gmail.com>
2018-12-28 15:04:22 -0800

    Formatting fixes

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -249,17 +249,17 @@ public class Driver {
         frame.repaint();
       }
     });
-    Integer[] positionsOptions = new Integer[] { 3, 4, 5, 6, 7, 8};
+    Integer[] positionsOptions = new Integer[] { 3, 4, 5, 6, 7, 8, 9, 10};
     JComboBox<Integer> numberPositions = new JComboBox<Integer>(positionsOptions);
     numberPositions.setFont(mainFont);
     numberPositions.setToolTipText("Number of positions per day.");
-    numberPositions.setSelectedIndex(3); // 6 positions is default
     numberPositions.addItemListener(new ItemListener() {
       @Override
       public void itemStateChanged(ItemEvent e) {
         Assigner.NUM_POSITIONS = (Integer)numberPositions.getSelectedItem();
       }
     });
+    numberPositions.setSelectedIndex(5); // 6 positions is default
     
     JPanel generatePanel = new JPanel();
     generatePanel.add(create);
@@ -786,9 +786,10 @@ public class Driver {
       fileOut.print("</title>\n");
       fileOut.print("<style type=\"text/css\">\n");
       fileOut.print("th, td { padding:1px; padding-right: 20px; width: 230px; font-weight: bold;font-size: 18px; vertical-align: top;}\n");
+      fileOut.print("li { font-weight: normal;font-size: 16px; }\n");
       fileOut.print("td { height: 139px;}\n");
       fileOut.print("h2 {text-align: center;}\n");
-      fileOut.print("ol { -webkit-margin-before: 5px; -webkit-margin-after: 5px; }\n");
+      fileOut.print("ol { -webkit-margin-before: 5px; -webkit-margin-after: 5px; margin-block-start: 0.1em; margin-block-end: 0.1em;}\n");
       fileOut.print("table, th, td {border: 1px solid black;border-collapse: collapse;}\n");
       fileOut.print("</style>\n");
       fileOut.print("</head>\n");
@@ -821,9 +822,16 @@ public class Driver {
           }
           else if( !days[week][day].isUnused() ) {
             fileOut.print("<ol>\n");
-            for( Employee e : days[week][day].getAssignments() ) {
+            List<Employee> assigned = days[week][day].getAssignments();
+            for(int position = 0; position < assigned.size(); position++) {
+              Employee e = assigned.get(position);
               fileOut.print("<li>");
               fileOut.print(e.getName());
+              if( position == 2 || position == 6 ) {
+                position++;
+                Employee b = assigned.get(position);
+                fileOut.print(" " + b.getName());
+              }
               fileOut.print("</li>\n");
             }
             fileOut.print("</ol>\n");
